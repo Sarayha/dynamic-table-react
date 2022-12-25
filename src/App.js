5c22af08d573778ec08a1ce17f19aa62408b0067
Sarayha <saraha@post.jce.ac.il>
2022-12-25 23:37:48 +0200

    DynamicTable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './mock-data.json';
+import {nanoid} from 'nanoid';
 
-function App() {
+const App = () => {
+
+  const[contacts, setContacts] = useState(data);
+  const[addFormData, setAddFormData] = useState({
+    name:'',
+    address:'',
+    phone:'',
+    email:'',
+  })
+
+  const handleAddFormChange = (event) =>{
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute('name');
+    const fieldValue = event.target.value;
+
+    const newFormData = {...addFormData};
+    newFormData[fieldName] = fieldValue;
+
+
+    setAddFormData(newFormData);
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: addFormData.name,
+      address: addFormData.address,
+      phone: addFormData.phone,
+      email: addFormData.email,
+    };
+
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <h1>contact</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone number</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact)=>(
+          <tr>
+            <td> {contact.name}</td>
+            <td>{contact.address.city}</td>
+            <td>{contact.phone}</td>
+            <td>{contact.email}</td>
+          </tr>
+
+          ))}
+
+
+
+
+        </tbody>
+      </table>
+      <h2>Add a Contact</h2>
+
+      <form onSubmit={handleAddFormSubmit}>
+        <input type="text" 
+        name='name' 
+        required = 'required' 
+        placeholder='Enter a name...'
+        onChange={handleAddFormChange}
+        />
+        <input type= "text" 
+        name= "address" 
+        required = "required" 
+        placeholder="Enter an address..."
+        onChange={handleAddFormChange}
+        />
+        <input type="text" 
+        name= "phone"
+        required = 'required' 
+        placeholder="Enter a phone number..."
+        onChange={handleAddFormChange}
+        />
+        <input type= "email" 
+        name= "email"
+        required = 'required' 
+        placeholder="Enter an email..."
+        onChange={handleAddFormChange}
+        />
+        <button type="Submit">Add</button>
+      </form>
+
     </div>
   );
-}
+};
 
 export default App;
